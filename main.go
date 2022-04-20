eda22c5f0a8f3b60514d54ca545db58bd3245b7b
GuillemXanxo <jscodebaker@gmail.com>
2022-04-20 13:06:22 +0200

    Imported mux with go.mod and created Viatge struct

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"github.com/gorilla/mux"
+)
+
+type Viatge struct {
+  origen string `json:origen`
+  desti string `json:desti`
+  places int `json:places`
+  dataNumber int
+  data string
+  horaSortidaNumber int
+  horaSortida string
+  comentaris string
+  dones bool
+}
+
 func main() {
-  
+  mux.NewRouter()
 }
